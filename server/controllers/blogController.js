3e49aa7cc2532fa1176028d791f2b546a866c476
abhishek10600 <abhi10sheksharma@gmail.com>
2023-11-03 15:17:14 +0530

    Blog controller completed.

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,6 +1,31 @@
 import Blog from "../models/blogModel.js";
 import { v2 as cloudinary } from "cloudinary";
 
+export const getAllBlogs = async (req, res, next) => {
+    try {
+        const blogs = await Blog.find();
+        res.status(200).json({
+            success: true,
+            blogs
+        })
+    } catch (error) {
+        return next(new Error(error.message));
+    }
+}
+
+export const getLoggedInUserBlogs = async (req, res, next) => {
+    try {
+        const userId = req.user.id;
+        const blogs = await Blog.find({ user: userId });
+        res.status(200).json({
+            success: true,
+            blogs
+        })
+    } catch (error) {
+        return next(new Error(error.message));
+    }
+}
+
 export const createBlog = async (req, res, next) => {
     try {
         const { title, description } = req.body;
@@ -42,5 +67,77 @@ export const createBlog = async (req, res, next) => {
         }
     } catch (error) {
         return next(new Error(error.message));
+    }
+}
+
+export const updateBlog = async (req, res, next) => {
+    const { blogId } = req.params;
+    let blog = await Blog.findById(blogId);
+    if (!blog) {
+        return next(new Error("Blog does not exists"));
+    }
+    const { title, description } = req.body;
+    if (req.files) {
+        let imagesArray = [];
+        for (let index = 0; index < blog.photos.length; index++) {
+            const res = await cloudinary.uploader.destroy(blog.photos[index].id);
+        }
+        for (let index = 0; index < req.files.photos.length; index++) {
+            let result = await cloudinary.uploader.upload(req.files.photos[index].tempFilePath, {
+                folder: "blogapp/blogs"
+            });
+            imagesArray.push({
+                id: result.public_id,
+                secure_url: result.secure_url
+            })
+        }
+        req.body.photos = imagesArray;
+        blog = await Blog.findByIdAndUpdate(blogId, req.body, {
+            new: true,
+            runValidators: true,
+            useFindAndModify: false
+        });
+        res.status(200).json({
+            success: true,
+            message: "Blog successfully updated",
+            blog
+        })
+
+    } else {
+        blog.title = title;
+        blog.description = description;
+        await blog.save();
+        res.status(200).json({
+            success: true,
+            message: "Blog updated successfully!",
+            blog
+        })
+    }
+}
+
+export const deleteBlog = async (req, res, next) => {
+    const { blogId } = req.params;
+    if (!blogId) {
+        return next(new Error("Blog does not exists"));
+    }
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+        return next(new Error("Blog does not exists"));
+    }
+    if (blog.photos.length === 0) {
+        await blog.deleteOne();
+        res.status(200).json({
+            success: true,
+            message: "Blog deleted successfully"
+        })
+    } else {
+        for (let index = 0; index < blog.photos.length; index++) {
+            const res = await cloudinary.uploader.destroy(blog.photos[index].id);
+        }
+        await blog.deleteOne();
+        res.status(200).json({
+            success: true,
+            message: "Blog deleted successfully"
+        })
     }
 }
--- server/routes/blogRouter.js
+++ server/routes/blogRouter.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { createBlog } from "../controllers/blogController.js";
+import { createBlog, getAllBlogs, getLoggedInUserBlogs, updateBlog, deleteBlog } from "../controllers/blogController.js";
 import { isLoggedIn } from "../middlewares/userMiddleware.js";
 
 
 const router = express.Router();
 
 router.route("/createBlog").post(isLoggedIn, createBlog);
+router.route("/all").get(getAllBlogs);
+router.route("/myBlogs").get(isLoggedIn, getLoggedInUserBlogs);
+router.route("/updateBlog/:blogId").put(isLoggedIn, updateBlog);
+router.route("/deleteBlog/:blogId").delete(isLoggedIn, deleteBlog);
 
 
 export default router;
