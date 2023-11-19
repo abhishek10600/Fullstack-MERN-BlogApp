87bd108a130526ba6a5b761027445c262331f006
abhishek10600 <abhi10sheksharma@gmail.com>
2023-11-19 11:28:03 +0530

    Login frontend connected with login controller and login functionality is working.

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,20 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLoginFormSubmit = async (ev) => {
+    ev.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("email", email);
+      formData.append("password", password);
+      const res = await axios.post(
+        "http://localhost:4000/api/v1/users/login",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      if (res.data.success === true) {
+        alert(res.data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="form-container md:w-full md:my-20">
-      <form className="flex flex-col md:gap-8 gap-6 md:my-5 mx-4">
+      <form
+        className="flex flex-col md:gap-8 gap-6 md:my-5 mx-4"
+        onSubmit={handleLoginFormSubmit}
+      >
         <input
           type="email"
           className="p-4 sm:p-5 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:focus:ring-gray-600"
           placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           className="p-4 sm:p-5 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:focus:ring-gray-600"
-          placeholder="Create a password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button
-          type="button"
+          type="submit"
           className="w-full py-4 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
         >
           Login
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,4 +1,3 @@
-
 import LoginForm from "../components/LoginForm";
 
 const LoginPage = () => {
